3c5ece45b9bcbc083e6c31035adf5a0a63b9b0c4
Abimael M <abimaelmercado05@gmail.com>
2023-04-09 23:45:35 -0400

    translate texts to whale language

--- Whale-Translator/ViewController.swift
+++ Whale-Translator/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var OutputLabel: UILabel!
+    @IBOutlet weak var translateButton: UIButton!
+    
+    @IBOutlet weak var inputField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func translateTheText(_ sender: Any) {
+        let input: String = inputField.text ?? ""
+        var output: String = ""
+        
+        for char in input {
+        let lowerChar = char.lowercased()
+         
+         switch lowerChar {
+           case "a", "i", "o":
+           output += lowerChar.uppercased()
+           case "e":
+           output += "EE"
+           case "u":
+           output += "UU"
+           default:
+           continue
+         }
+        }
+        OutputLabel.text = output
+    }
+    
 }
 
