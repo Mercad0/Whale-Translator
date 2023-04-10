5cd43c660f777e863235126b7a10f76cac00a11a
Abimael M <abimaelmercado05@gmail.com>
2023-04-09 22:33:20 -0400

    Initial Commit

---
+++ Whale-Translator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whale-Translator
+//
+//  Created by AM on 4/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
